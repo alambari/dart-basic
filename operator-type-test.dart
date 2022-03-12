f5f04208fe66655c0ec342a687379b8f4a352113
Al Ambari <al.ambari@usetada.com>
2022-03-12 15:18:20 +0700

    learn operator type test

---
+++ operator-type-test.dart
@@ -0,0 +1,23 @@
+void main() {
+
+
+  dynamic variable = 123;
+
+  // var variableString = variable as String; // Error
+  var variableInt = variable as int;
+
+  var isInt = variable is int;
+  var isNotBool = variable is! bool;
+
+  print(variableInt);
+  print(isInt);
+  print(isNotBool);
+
+  print(variable is bool);
+  print(variable is String);
+  print(variable is Object);
+
+  print(variable is! bool);
+  print(variable is! String);
+  print(variable is! int);
+}
